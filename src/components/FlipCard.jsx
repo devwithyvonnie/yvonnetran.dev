fbd45f7497edcd00672118097781ebe9f4a7a5ca
devwithyvonnie <devwithyvonnie@gmail.com>
2025-08-04 09:28:50 -0700

    Updated FlipCard.jsx to include the intended extra link.

--- src/components/FlipCard.jsx
+++ src/components/FlipCard.jsx
@@ -1,19 +1,23 @@
 import styles from "../styles/FlipCard.module.css";
 
-function FlipCard({ image, title, description, link, linkLabel }) {
+function FlipCard({ image, title, description, links }) {
   return (
     <div className={styles.card}>
       <img src={image} alt={title} className={styles.image} />
       <h3 className={styles.title}>{title}</h3>
       <p className={styles.description}>{description}</p>
-      <a
-        className={styles.link}
-        href={link}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        {linkLabel}
-      </a>
+      <div className={styles.links}>
+        {links.map((l, i) => (
+          <a
+            key={i}
+            href={l.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.link}>
+            {l.label}
+          </a>
+        ))}
+      </div>
     </div>
   );
 }

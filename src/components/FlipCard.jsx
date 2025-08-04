7ed9ea57bebf2b4e389cf1ee92a8e26909a51735
devwithyvonnie <devwithyvonnie@gmail.com>
2025-08-04 09:56:52 -0700

    100th times the charm?

--- src/components/FlipCard.jsx
+++ src/components/FlipCard.jsx
@@ -1,29 +1,27 @@
+import React from "react";
 import styles from "../styles/FlipCard.module.css";
 
-const FlipCard = ({ title, image, description, links }) => {
+function FlipCard({ image, title, description, links }) {
   return (
     <div className={styles.card}>
       <img src={image} alt={title} className={styles.image} />
       <h3 className={styles.title}>{title}</h3>
       <p className={styles.description}>{description}</p>
-
-      {Array.isArray(links) && links.length > 0 && (
-        <div className={styles.links}>
-          {links.map((l, index) => (
-            <a
-              key={index}
-              href={l.url}
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.link}
-            >
-              {l.label}
-            </a>
-          ))}
-        </div>
-      )}
+      <div className={styles.links}>
+        {links?.map((link, index) => (
+          <a
+            key={index}
+            href={link.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.link}
+          >
+            {link.label}
+          </a>
+        ))}
+      </div>
     </div>
   );
-};
+}
 
 export default FlipCard;
